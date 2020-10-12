20ffe85bf8835bccdb6a3ffea13af63ae2e044e7
mikipmax <pmax-111@hotmail.es>
2020-10-12 00:07:07 -0500

    config

---
+++ spring-config-server/src/main/java/com/distribuida/SpringConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.distribuida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+
+}
